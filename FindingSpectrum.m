cdab49943df32c15249cc3ef2c1076854f7aea49
amukher3 <Abhishek.Mukherjee@rockets.utoledo.edu>
2022-01-15 23:17:46 -0800

    Update FindingSpectrum.m
    The lower bound on the higherCutOffFreqn was changed to 0.05Hz and the granularity of the decmtFactor was changed to 0.1 the granularity of the phaseShift was changed to 1/100

--- FindingSpectrum.m
+++ FindingSpectrum.m
@@ -1,9 +1,47 @@
-% Pxx -- Power spectrum of the Tachogram
-% Pyy -- Power spectrum of the estiamted RSA
-% TimeSig -- Tachogram for a particular user
+
+
 
 function [pxx,pyy,f,yfit]=...
-    FindingSpectrum(TimeSig,SamplingFreqn)
+    FindingSpectrum(TimeSig,SamplingFreqn,...
+    HighFreqnCut1,HighFreqnCut2)
+
+%#######################################################
+%#######################################################
+
+% This function computes the RSA estimte for a given HR
+% time series. It uses the technique of Matching pursuit 
+% to fit a bunch of sinusoids to extract the Respiratory
+% component affecting the Tachogram. Since the respiratory 
+% component affecting the tachogram is just a linear 
+% addition it does well in extracting the respiratory
+% component. 
+
+% The dictionary consists of a bunch of sinusoids of 
+% varying frequency ranges within the allowable range.
+% Phase shifts from 0 to 2pi are given to every sinsusoid
+% at a particular frequency to capture the phase
+% misalignment. 
+
+%########################################################
+%########################################################
+
+
+
+%########################################################
+%########################################################
+
+% N.B: Other methods of removing the respiraroty component
+% can also be used. For example, a simple low pass filter 
+% can remove the High Frequency component as well. 
+
+%##########################################################
+%##########################################################
+
+
+
+% Pxx -- Power spectrum of the Tachogram
+% Pyy -- Power spectrum of the estiamted RSA
+% TimeSig -- HR series for a particular window
 
 
 %%% Building the dictionary and performing MP %%%
@@ -11,81 +49,84 @@ function [pxx,pyy,f,yfit]=...
 % Time bins for the time signal
 t=1:1:length(TimeSig);
 
-%DecmtFact=10:5:50;
-%DecmtFact=35:5:80;
-%DecmtFact=35:1:50;
-%DecmtFact=35:0.5:50;
-%DecmtFact=50:0.5:52;
-%DecmtFact=20:2:62;
-%DecmtFact=20:2:60;
-%DecmtFact=40:5:50;
+%##################################################
+% Bounds for the frerquency decimation
+% calculated on the basis of the sampling freqn,
+% the length of the timeSignal
+% and the Freqn ranges.
+% For example, if 'n'is the number of samples in 
+% the signal at 'f' Hz then we can calculate the 
+% amount of decimation needed to make it lie between 
+% the allowable frequency ranges generally considered
+% for the respiratory component. 
+%###################################################
 
-%Decimating Factor -- decides the amount of frequency decimation..
-%increasing the bound moves the CF peak position to a higher value.
+%Bounds for the amount of decimation needed
+%to get it to the upper bound. 
+FinalBound=SamplingFreqn*HighFreqnCut2;
+InitialBound=SamplingFreqn*HighFreqnCut1;
 
-%Increasing the final bound affects the HF power more 
-%
-FinalBound=150;
-InitialBound=1;
+% Amount of frequency decimation from the original signal
+% to build the dictionary%
+DecmtFact=InitialBound:.1:FinalBound;
 
-%Amount of Decimation in the dictionary
-DecmtFact=InitialBound:1:FinalBound; 
+%Phase shifts at a particualr frequency%
+PhaseShift=0:pi/100:2*pi;
 
-Amp=0.5:0.5:10;
+%%Pre-allocating the dictionary
+rsaDict = zeros(length(TimeSig), length(PhaseShift)*length(DecmtFact));
 
-%%%  Buliding the dictionary %%
+%%  Buliding the dictionary %%%
+
 Count=1;
 
 for j=1:length(DecmtFact)
-%     for k=1:length(Amp)
+
+    for PhaseIdx=1:length(PhaseShift)
         
     rsaDict(:,Count)=...
-        1*(sin(t/DecmtFact(j)));
-    Count=Count+1;
+    cos(t/DecmtFact(j)+PhaseShift(PhaseIdx));
+
     
-%     end
-end
+     Count=Count+1;
+    
+     end
+ end
 
 [yfit,r,coeff,iopt,qual]=wmpalg('OMP',TimeSig,rsaDict);
 
-%yfit=r;
-%%% DownSampling the RR series %%%
-% Needed only if we want to downsample to a lower
-% Frequency than the original frequency
-
-% DownSamFreq=4;
-% TimeSig2=downsample(TimeSig,round(SamplingFreqn/DownSamFreq));
-
-
-%yfit is the estimated RSA. 
-% plot(yfit); 
-% figure
-
 % frequency bins
-f=0:0.001:1; 
+f=0:0.01:1; 
 
 % Power spectrum for the  estimated RSA
 pyy=pwelch(yfit,[],[],f,SamplingFreqn);
+close;
+plot(f,pyy,'*');
+hold
 
-% plot(f,pyy,'*');
-% hold
+% Definining the BPF object
+bpFilt = designfilt('bandpassfir','FilterOrder',200, ...
+'CutoffFrequency1',0.03,'CutoffFrequency2',0.7,...
+'SampleRate',SamplingFreqn);
 
-% % Definining the BPF object
-% bpFilt = designfilt('bandpassfir','FilterOrder',200, ...
-% 'CutoffFrequency1',0.05,'CutoffFrequency2',0.7,...
-% 'SampleRate',SamplingFreqn);
-% 
-% % Performing filtering 
-% dataOut=filter(bpFilt,TimeSig);
+%Performing filtering as a pre-processing step
+% as suggested in the literature...
+dataOut=filter(bpFilt,TimeSig);
 
 % Power spectrum of the original tachigram
+% the time window of the original HR 
+% series.
 pxx=pwelch(TimeSig,[],[],f,SamplingFreqn);
+
+ plot(f,pxx,'--');
+ legend('Spectrum of the RSA component','Spectrum of the Original Tachogram');
+ xlim([0.15 1])
+ ylim([0,2.5e-2])
+ xlabel('Frequency in Hz');
+ ylabel('Power in S^2/Hz');
 
 
 end
 
 % plot(f,pxx,'*');
-% legend('Spectrum of the RSA component','Spectrum of the Original Tachogram');
-% xlim([0.15 1])
-% xlabel('Frequency in Hz');
-% ylabel('Power in S^2/Hz');
+
